797ab65608d6f2b5908f9a46839f747335e64ddc
Pushkar Kadam <pushkarkadam17@outlook.com>
2025-05-18 17:09:50 +1000

    meshes added for pointer

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
